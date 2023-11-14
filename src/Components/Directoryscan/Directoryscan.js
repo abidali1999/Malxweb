650537658babd5d31b3dbe3b74d8f51947ad4ef8
abidali1999 <abidali1999063@gmail.com>
2023-11-14 11:24:55 +0500

    changes in file upload and directory scan

--- src/Components/Directoryscan/Directoryscan.js
+++ src/Components/Directoryscan/Directoryscan.js
@@ -1,104 +1,198 @@
-import React, { useState, useRef } from 'react';
-import ProgressBar from './ProgressBar';
+import React, { useState, useRef,useEffect  } from 'react';
 import './Directoryscan.css';
 import searchfile from '../../assets/icons/search-file.png';
 import { Link } from 'react-router-dom';
 import Ctitle from '../Ctitle/Ctitle';
 
 function Directoryscan() {
-
+  const fileInputRef = useRef(null);
   const [selectedDirectory, setSelectedDirectory] = useState(null);
-  const [showProgressBar, setShowProgressBar] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [predictionResults, setPredictionResults] = useState({});
+  const [validFilePaths, setValidFilePaths] = useState([]);
 
-  // const handleDirectoryChange = (e) => {
-  //   const files = e.target.files;
-  //   setSelectedDirectory(files.length > 0 ? files[0].webkitRelativePath : null);
-  // };
-
-  const handleDirectoryChange = (e) => {
-    const files = e.target.files;
-    const path = files.length > 0 ? files[0].webkitRelativePath : null;
-    setSelectedDirectory(path);
+  const opendirectory = () => {
+    fileInputRef.current.click();
   };
 
+  const handleDirectoryChange = (event) => {
+    const files = event.target.files;
+    console.log("total files: ",files.length)
+    if (files.length > 0) {
+      const directoryPath = files[0].webkitRelativePath;
+      setSelectedDirectory(directoryPath);
 
+      // Get the list of valid file paths
+      const fileList = Array.from(files);
+      const maxFileSize = 3 * 1024 * 1024;
 
-  const handleClick = () => {
-    setShowProgressBar(!showProgressBar);
-  };
+      const validFiles = fileList.filter(
+        (file) =>
+          (file.name.endsWith('.dll') || file.name.endsWith('.exe')) &&
+          file.size <= maxFileSize
+      );
 
-  // Update your handleClick function to send a request to the server
-const handlesubmit = async () => {
-  setShowProgressBar(!showProgressBar);
-
-  if (selectedDirectory) {
-    try {
-      // Send a POST request to your server to save the directory path
-      await fetch('http://localhost:5000/api/directory/save-path', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ path: selectedDirectory }),
-      });
-    } catch (error) {
-      console.error('Error saving directory path:', error);
+      const validPaths = validFiles.map((file) => URL.createObjectURL(file));
+      setValidFilePaths(validPaths);
     }
-  }
-};
+    // console.log("valid paths: ",validFilePaths.length)
+    
+  };
+  useEffect(() => {
+    // This code will run after the component re-renders
+    console.log("valid paths: ", validFilePaths.length);
+  }, [validFilePaths]); 
+  const handleClick = async () => {
+    if (selectedDirectory && validFilePaths.length > 0) {
+      setIsLoading(true);
+      setProgress(0);
+      setPredictionResults({});
+      const suspiciousFiles = [];
+
+      const promises = validFilePaths.map(async (filePath, index) => {
+        const response = await fetch(filePath);
+        const blob = await response.blob();
+        const file = new File([blob], blob.name || `unknownFileName_${index}`);
+        await createAndSendByteplot(file, index);
+
+        if (
+          predictionResults[file.name] &&
+          predictionResults[file.name] !== 'benign'
+        ) {
+          suspiciousFiles.push(filePath);
+        }
+      });
+
+      try {
+        // Wait for all predictions to complete
+        await Promise.all(promises);
+      } catch (error) {
+        console.error('An error occurred during predictions:', error);
+      } finally {
+        setIsLoading(false);
+        setProgress(100);
+
+        // Display results
+        if (suspiciousFiles.length === 0) {
+          console.log('Scan complete, no suspicious files found');
+        } else {
+          console.log('Suspicious files found:', suspiciousFiles);
+        }
+      }
+    }
+  };
+
+  const createAndSendByteplot = async (file, index) => {
+    if (!file.name) {
+      console.error('File name is undefined:', file);
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await fetch(
+        'https://abidali1999063.pythonanywhere.com/predict',
+        {
+          method: 'POST',
+          body: formData,
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setPredictionResults((prevResults) => ({
+          ...prevResults,
+          [file.name]: data.predicted_class,
+        }));
+      } else {
+        console.error('Prediction request failed for file:', file.name);
+      }
+    } catch (error) {
+      console.error('An error occurred during prediction:', error);
+    }
+
+    // Update the progress
+    // console.log("prev progress: ",prevProgress)
+    setProgress((prevProgress) => (prevProgress+1)*(100 / validFilePaths.length));
+  };
 
   return (
     <>
-
       <Ctitle title="Directory Scan" />
       <div className='container'>
         <div className='row'>
           <div className='col-md-4'></div>
-          <div className='col-md-4' style={{ textAlign: "center" }}>
+          <div className='col-md-4' style={{ textAlign: 'center' }}>
+            <img src={searchfile} style={{ width: '50%' }} />
+            <br />
+            <label htmlFor='directoryUpload'>Select Directory</label>
+            <input
+              type='file'
+              id='directoryUpload'
+              webkitdirectory='true'
+              directory='true'
+              ref={fileInputRef}
+              onChange={handleDirectoryChange}
+            />
 
-            {showProgressBar && (
-              <div className="progress" style={{ marginTop: "50px" }}>
-                <div className="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style={{ width: '25%' }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                  25%
+            {selectedDirectory && (
+              <div>
+                <p>Selected Directory:</p>
+                <p>{selectedDirectory}</p>
+              </div>
+            )}
+            {/* <button
+              className='btn btn-dark mt-2'
+              onClick={handleClick}
+              disabled={isLoading}
+            >
+              Scan
+            </button> */}
+            {isLoading && (
+              <div className='progress' style={{ marginTop: '20px' }}>
+                <div
+                  className='progress-bar progress-bar-striped progress-bar-animated'
+                  role='progressbar'
+                  style={{ width: `${progress}%` }}
+                  aria-valuenow={progress}
+                  aria-valuemin='0'
+                  aria-valuemax='100'
+                >
+                  {Math.round(progress)}%
                 </div>
               </div>
-
             )}
-            <img src={searchfile} style={{ width: "50%" }} /><br />
-            <form onSubmit={handlesubmit}>
-              <label htmlFor="directoryUpload">Select Directory</label>
-              <input
-                type="file"
-                id="directoryUpload"
-                webkitdirectory="true"
-                directory="true" // For non-Chrome browsers
-                onChange={handleDirectoryChange}
-              />
-
-              {selectedDirectory && (
-                <div>
-                  <p>Selected Directory:</p>
-                  <p>{selectedDirectory}</p>
-                </div>
-              )}
-              <button className='btn btn-dark mt-2'>Save path</button>
-            </form>
-
           </div>
           <div className='col-md-4'></div>
         </div>
 
-        <div className='row' style={{ marginTop: "50px", marginBottom: "20px" }}>
-          <div className='col-md-6' style={{ textAlign: "center" }}>
-            <button className='btn btn-dark' style={{ width: "40%" }} onClick={handleClick}> {showProgressBar ? 'Cancel' : 'Scan'}</button>
+        <div className='row' style={{ marginTop: '50px', marginBottom: '20px' }}>
+          <div className='col-md-6' style={{ textAlign: 'center' }}>
+            <button
+              className='btn btn-dark'
+              style={{ width: '40%' }}
+              onClick={handleClick}
+              disabled={isLoading}
+            >
+              Scan
+            </button>
           </div>
-          <div className='col-md-6' style={{ textAlign: "center" }}>
-            <Link className='btn btn-outline-dark' style={{ width: "40%" }} to="/dashboard">BACK</Link>
+          <div className='col-md-6' style={{ textAlign: 'center' }}>
+            <Link
+              className='btn btn-outline-dark'
+              style={{ width: '40%' }}
+              to='/dashboard'
+            >
+              BACK
+            </Link>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Directoryscan
+export default Directoryscan;
--- src/Components/Dragdrop/Dragdrop.js
+++ src/Components/Dragdrop/Dragdrop.js
@@ -1,140 +1,93 @@
-// import React,{useState,useRef} from 'react';
-// import './Dragdrop.css';
-
-// function Dragdrop() {
-
-//     const [files, setFiles] = useState(null);
-//     const inputRef = useRef()
-
-//     const handledragover=(event)=>{
-//         event.preventDefault();
-//     }
-
-//     const handledrop=(event)=>{
-//         event.preventDefault();
-//         setFiles(event.dataTransfer.files)
-//     }
-
-//     const handleUpload = () => {
-//         const formData = new FormData();
-//         formData.append("Files", files);
-//         console.log(formData.getAll())
-//         // fetch(
-//         //   "link", {
-//         //     method: "POST",
-//         //     body: formData
-//         //   }  
-//         // )
-//       };
-
-//     if (files) return(
-//         <div className='uploads'>
-//             <ul>
-//                 {Array.from(files).map((files, idx)=> <li key={idx}>{files.name}</li>)}
-//             </ul>
-//             <div className="actions">
-//             <button onClick={() => setFiles(null)}>Cancel</button>
-//             <button onClick={handleUpload}>Upload</button>
-//         </div>
-//         </div>
-//     )
-//   return (
-//     <>
-//       {
-//         !files && 
-//         <div className='dropzone' onDragOver={handledragover} onDrop={handledrop}>
-//             <h3>Drag & Drop files to upload</h3>
-//             <h3>Or</h3>
-//             <input type='file' multiple onChange={(event)=>setFiles(event.target.files)} hidden 
-//             ref={inputRef}/> 
-//             <button className='btn btn-dark' onClick={()=>inputRef.current.click()}>Select Files</button>
-//         </div>
-//       }
-//     </>
-//   )
-// }
-
-// export default Dragdrop
-
-
-// Dragdrop.js
-
-import React, { useState, useRef } from 'react';
+import React,{useState,useRef} from 'react';
 import './Dragdrop.css';
 
 function Dragdrop() {
-  const [files, setFiles] = useState(null);
-  const inputRef = useRef();
 
-  const handledragover = event => {
-    event.preventDefault();
-  };
+    const [files, setFiles] = useState(null);
+    const inputRef = useRef()
 
-  const handledrop = event => {
-    event.preventDefault();
-    setFiles(event.dataTransfer.files);
-  };
-
-  const handleUpload = async () => {
-    const formData = new FormData();
-    Array.from(files).forEach(file => {
-      formData.append('Files', file);
-    });
-
-    try {
-      const response = await fetch('http://localhost:5000/upload', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        const result = await response.json();
-        console.log('Files uploaded successfully:', result.files);
-        setFiles(null);
-      } else {
-        console.error('Failed to upload files');
-      }
-    } catch (error) {
-      console.error('Error:', error);
+    const handledragover=(event)=>{
+        event.preventDefault();
     }
-  };
 
-  if (files)
-    return (
-      <div className='uploads'>
-        <ul>
-          {Array.from(files).map((file, idx) => (
-            <li key={idx}>{file.name}</li>
-          ))}
-        </ul>
-        <div className='actions'>
-          <button onClick={() => setFiles(null)}>Cancel</button>
-          <button onClick={handleUpload}>Upload</button>
+    const handledrop=(event)=>{
+        event.preventDefault();
+        setFiles(event.dataTransfer.files)
+    }
+
+    // const handleUpload = () => {
+    //     const formData = new FormData();
+    //     formData.append("Files", files);
+    //     console.log(formData.getAll())
+    //     // fetch(
+    //     //   "link", {
+    //     //     method: "POST",
+    //     //     body: formData
+    //     //   }  
+    //     // )
+    //   };
+    const handleUpload = async () => {
+      if (files && files.length > 0) {
+        const formData = new FormData();
+        formData.append('file', files[0]); // Assuming you want to process the first file
+    
+        try {
+          // Send the file to your API for prediction
+          const response = await fetch('https://abidali1999063.pythonanywhere.com/predict', {
+            method: 'POST',
+            body: formData,
+          });
+          console.log(response.status)
+          console.log(response.statusText)
+          console.log(response.text)
+    
+          if (response.ok) {
+            // Successfully received a response from the API
+            const jsonResponse = await response.json();
+            const result = jsonResponse.predicted_class;
+            alert(`Predicted class: ${result}`);
+            setFiles(null); // Clear the selected file(s) state
+          } else {
+            // Handle API response error
+            alert('Prediction failed. Please try again.');
+          }
+        } catch (error) {
+          // Handle any network or API request errors
+          console.error('API request failed:', error);
+          alert('Prediction request failed. Please try again.');
+        }
+      } else {
+        // No file selected, handle accordingly
+        alert('No file selected for prediction.');
+      }
+    };
+    
+
+    if (files) return(
+        <div className='uploads'>
+            <ul>
+                {Array.from(files).map((files, idx)=> <li key={idx}>{files.name}</li>)}
+            </ul>
+            <div className="actions">
+            <button onClick={() => setFiles(null)}>Cancel</button>
+            <button onClick={handleUpload}>Upload</button>
         </div>
-      </div>
-    );
-
+        </div>
+    )
   return (
     <>
-      {!files && (
+      {
+        !files && 
         <div className='dropzone' onDragOver={handledragover} onDrop={handledrop}>
-          <h3>Drag & Drop files to upload</h3>
-          <h3>Or</h3>
-          <input
-            type='file'
-            multiple
-            onChange={event => setFiles(event.target.files)}
-            hidden
-            ref={inputRef}
-          />
-          <button className='btn btn-dark' onClick={() => inputRef.current.click()}>
-            Select Files
-          </button>
+            <h3>Drag & Drop files to upload</h3>
+            <h3>Or</h3>
+            <input type='file' multiple onChange={(event)=>setFiles(event.target.files)} hidden 
+            ref={inputRef}/> 
+            <button className='btn btn-dark' onClick={()=>inputRef.current.click()}>Select Files</button>
         </div>
-      )}
+      }
     </>
-  );
+  )
 }
 
-export default Dragdrop;
-
+export default Dragdrop
